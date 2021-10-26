3ef93a636bf9b88ddd8e8bac29075d8e591516fc
Aleksander Kruse Schaumburg-Müller <alek1082@edu.easj.dk>
2021-10-26 14:31:07 +0200

    Add project files.

---
+++ UDPClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UDP Client!");
+
+            byte[] data = Encoding.UTF8.GetBytes("Hej med jer alle");
+
+            using (UdpClient socket = new UdpClient())
+            {
+                socket.Send(data, data.Length, "127.0.0.1", 5006);
+            }
+
+
+        }
+    }
+}
---
+++ UDPServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UDP Server!");
+
+
+           using ( UdpClient socket = new UdpClient())
+            { 
+
+                socket.Client.Bind(new IPEndPoint(IPAddress.Any, 5006));
+            
+                while (true)
+                {
+                    IPEndPoint clientEndPoint = null;
+                    byte[] received = socket.Receive(ref clientEndPoint);
+                    Console.WriteLine("Client connected: " + clientEndPoint.Address);
+                    Console.WriteLine("Data: " + Encoding.UTF8.GetString(received));
+
+                }
+
+            }
+        }
+    }
+}
